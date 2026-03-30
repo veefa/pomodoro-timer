09dbefd3481064b4fb3495f45dab90d7503f56d4
vefa <44091688+veefa@users.noreply.github.com>
2026-03-30 10:43:32 +0100

    - dependicies update - home path fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,9 @@ import Notifications from "./components/Notification";
 import TaskList from "./components/TaskList";
 import Sidebar from "./components/Sidebar";
 import Profile from "./components/Profile";
-
+import Home from "./Pages/Home";
 
 // Placeholder components for routes that aren't implemented yet
-
-
 
 const App: React.FC = () => {
   return (
@@ -20,7 +18,6 @@ const App: React.FC = () => {
         {/* Header */}
         <header className="h-16 w-full bg-[#1f1f1f] flex items-center justify-between px-6 border-b border-gray-700">
           <h1 className="text-2xl font-bold">Pomodoro Timer</h1>
-          
         </header>
 
         {/* Main Content */}
@@ -30,10 +27,13 @@ const App: React.FC = () => {
           {/* Main Panel */}
           <main className="flex-1 p-6 flex flex-col overflow-auto">
             <Routes>
-             
+              <Route path="/" element={<TimerPanel />} />
+
+              
+              <Route path="/home" element={<Home />} />
               <Route path="/timer" element={<TimerPanel />} />
               <Route path="/tasks" element={<TaskList />} />
-             
+
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/notifications" element={<Notifications />} />
               <Route path="/profile" element={<Profile />} />
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TimerPanel from "../components/TimerPanel";
+
+const Home: React.FC = ()  => {
+    return ( 
+        <TimerPanel/>
+     );
+}
+ 
+export default Home;
